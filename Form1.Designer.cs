4c83c3f15842d8257637f2e750746a556d2a8443
zeyadmady <zeyad221101084@nmu.edu.eg>
2025-04-30 16:32:40 +0300

    first commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,235 @@
+﻿namespace CaptchaGenerator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.rbText = new System.Windows.Forms.RadioButton();
+            this.rbCheckbox = new System.Windows.Forms.RadioButton();
+            this.rbImage = new System.Windows.Forms.RadioButton();
+            this.rbMath = new System.Windows.Forms.RadioButton();
+            this.lblCaptcha = new System.Windows.Forms.Label();
+            this.pbCaptcha = new System.Windows.Forms.PictureBox();
+            this.cbCaptcha = new System.Windows.Forms.CheckBox();
+            this.txtAnswer = new System.Windows.Forms.TextBox();
+            this.btnSubmit = new System.Windows.Forms.Button();
+            this.btnRefresh = new System.Windows.Forms.Button();
+            this.panelCaptchaImages = new System.Windows.Forms.Panel();
+            this.lblInstruction = new System.Windows.Forms.Label();
+            this.labelResult = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pbCaptcha)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // rbText
+            // 
+            this.rbText.AutoSize = true;
+            this.rbText.Location = new System.Drawing.Point(172, 10);
+            this.rbText.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.rbText.Name = "rbText";
+            this.rbText.Size = new System.Drawing.Size(99, 17);
+            this.rbText.TabIndex = 0;
+            this.rbText.TabStop = true;
+            this.rbText.Text = "Text CAPTCHA";
+            this.rbText.UseVisualStyleBackColor = true;
+            this.rbText.Click += new System.EventHandler(this.rbText_CheckedChanged);
+            // 
+            // rbCheckbox
+            // 
+            this.rbCheckbox.AutoSize = true;
+            this.rbCheckbox.Location = new System.Drawing.Point(170, 108);
+            this.rbCheckbox.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.rbCheckbox.Name = "rbCheckbox";
+            this.rbCheckbox.Size = new System.Drawing.Size(126, 17);
+            this.rbCheckbox.TabIndex = 1;
+            this.rbCheckbox.TabStop = true;
+            this.rbCheckbox.Text = "Checkbox CAPTCHA";
+            this.rbCheckbox.UseVisualStyleBackColor = true;
+            this.rbCheckbox.Click += new System.EventHandler(this.rbCheckbox_CheckedChanged);
+            // 
+            // rbImage
+            // 
+            this.rbImage.AutoSize = true;
+            this.rbImage.Location = new System.Drawing.Point(170, 42);
+            this.rbImage.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.rbImage.Name = "rbImage";
+            this.rbImage.Size = new System.Drawing.Size(107, 17);
+            this.rbImage.TabIndex = 2;
+            this.rbImage.TabStop = true;
+            this.rbImage.Text = "Image CAPTCHA";
+            this.rbImage.UseVisualStyleBackColor = true;
+            this.rbImage.CheckedChanged += new System.EventHandler(this.rbImage_CheckedChanged);
+            this.rbImage.Click += new System.EventHandler(this.rbImage_CheckedChanged);
+            // 
+            // rbMath
+            // 
+            this.rbMath.AutoSize = true;
+            this.rbMath.Location = new System.Drawing.Point(170, 74);
+            this.rbMath.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.rbMath.Name = "rbMath";
+            this.rbMath.Size = new System.Drawing.Size(102, 17);
+            this.rbMath.TabIndex = 3;
+            this.rbMath.TabStop = true;
+            this.rbMath.Text = "Math CAPTCHA";
+            this.rbMath.UseVisualStyleBackColor = true;
+            this.rbMath.Click += new System.EventHandler(this.rbMath_CheckedChanged);
+            // 
+            // lblCaptcha
+            // 
+            this.lblCaptcha.AutoSize = true;
+            this.lblCaptcha.Location = new System.Drawing.Point(405, 392);
+            this.lblCaptcha.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblCaptcha.Name = "lblCaptcha";
+            this.lblCaptcha.Size = new System.Drawing.Size(35, 13);
+            this.lblCaptcha.TabIndex = 4;
+            this.lblCaptcha.Text = "label1";
+            // 
+            // pbCaptcha
+            // 
+            this.pbCaptcha.Location = new System.Drawing.Point(707, 64);
+            this.pbCaptcha.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.pbCaptcha.Name = "pbCaptcha";
+            this.pbCaptcha.Size = new System.Drawing.Size(120, 83);
+            this.pbCaptcha.TabIndex = 5;
+            this.pbCaptcha.TabStop = false;
+            this.pbCaptcha.Visible = false;
+            // 
+            // cbCaptcha
+            // 
+            this.cbCaptcha.AutoSize = true;
+            this.cbCaptcha.Location = new System.Drawing.Point(723, 202);
+            this.cbCaptcha.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.cbCaptcha.Name = "cbCaptcha";
+            this.cbCaptcha.Size = new System.Drawing.Size(93, 17);
+            this.cbCaptcha.TabIndex = 6;
+            this.cbCaptcha.Text = "I\'m not a robot";
+            this.cbCaptcha.UseVisualStyleBackColor = true;
+            // 
+            // txtAnswer
+            // 
+            this.txtAnswer.Location = new System.Drawing.Point(367, 370);
+            this.txtAnswer.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.txtAnswer.Name = "txtAnswer";
+            this.txtAnswer.Size = new System.Drawing.Size(257, 20);
+            this.txtAnswer.TabIndex = 7;
+            // 
+            // btnSubmit
+            // 
+            this.btnSubmit.Location = new System.Drawing.Point(367, 427);
+            this.btnSubmit.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnSubmit.Name = "btnSubmit";
+            this.btnSubmit.Size = new System.Drawing.Size(56, 19);
+            this.btnSubmit.TabIndex = 8;
+            this.btnSubmit.Text = "Submit";
+            this.btnSubmit.UseVisualStyleBackColor = true;
+            this.btnSubmit.Click += new System.EventHandler(this.btnSubmit_Click);
+            // 
+            // btnRefresh
+            // 
+            this.btnRefresh.Location = new System.Drawing.Point(427, 427);
+            this.btnRefresh.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnRefresh.Name = "btnRefresh";
+            this.btnRefresh.Size = new System.Drawing.Size(56, 19);
+            this.btnRefresh.TabIndex = 9;
+            this.btnRefresh.Text = "Refresh";
+            this.btnRefresh.UseVisualStyleBackColor = true;
+            this.btnRefresh.Click += new System.EventHandler(this.btnRefresh_Click);
+            // 
+            // panelCaptchaImages
+            // 
+            this.panelCaptchaImages.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panelCaptchaImages.Location = new System.Drawing.Point(353, 30);
+            this.panelCaptchaImages.Name = "panelCaptchaImages";
+            this.panelCaptchaImages.Size = new System.Drawing.Size(324, 329);
+            this.panelCaptchaImages.TabIndex = 10;
+            this.panelCaptchaImages.Paint += new System.Windows.Forms.PaintEventHandler(this.panelCaptchaImages_Paint);
+            // 
+            // lblInstruction
+            // 
+            this.lblInstruction.AutoSize = true;
+            this.lblInstruction.Location = new System.Drawing.Point(350, 9);
+            this.lblInstruction.Name = "lblInstruction";
+            this.lblInstruction.Size = new System.Drawing.Size(35, 13);
+            this.lblInstruction.TabIndex = 12;
+            this.lblInstruction.Text = "label1";
+            // 
+            // labelResult
+            // 
+            this.labelResult.AutoSize = true;
+            this.labelResult.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelResult.Location = new System.Drawing.Point(393, 483);
+            this.labelResult.Name = "labelResult";
+            this.labelResult.Size = new System.Drawing.Size(57, 20);
+            this.labelResult.TabIndex = 13;
+            this.labelResult.Text = "label1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoScroll = true;
+            this.ClientSize = new System.Drawing.Size(1026, 553);
+            this.Controls.Add(this.labelResult);
+            this.Controls.Add(this.lblInstruction);
+            this.Controls.Add(this.panelCaptchaImages);
+            this.Controls.Add(this.txtAnswer);
+            this.Controls.Add(this.btnRefresh);
+            this.Controls.Add(this.btnSubmit);
+            this.Controls.Add(this.cbCaptcha);
+            this.Controls.Add(this.pbCaptcha);
+            this.Controls.Add(this.lblCaptcha);
+            this.Controls.Add(this.rbMath);
+            this.Controls.Add(this.rbImage);
+            this.Controls.Add(this.rbCheckbox);
+            this.Controls.Add(this.rbText);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Name = "Form1";
+            this.Text = "Submit";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pbCaptcha)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RadioButton rbText;
+        private System.Windows.Forms.RadioButton rbCheckbox;
+        private System.Windows.Forms.RadioButton rbImage;
+        private System.Windows.Forms.RadioButton rbMath;
+        private System.Windows.Forms.Label lblCaptcha;
+        private System.Windows.Forms.PictureBox pbCaptcha;
+        private System.Windows.Forms.CheckBox cbCaptcha;
+        private System.Windows.Forms.TextBox txtAnswer;
+        private System.Windows.Forms.Button btnSubmit;
+        private System.Windows.Forms.Button btnRefresh;
+        private System.Windows.Forms.Panel panelCaptchaImages;
+        private System.Windows.Forms.Label lblInstruction;
+        private System.Windows.Forms.Label labelResult;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,330 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace CaptchaGenerator
+{
+    public partial class Form1 : Form
+    {
+        private string currentCaptcha = "";
+        private Random random = new Random();
+
+        private List<PictureBox> captchaPictures = new List<PictureBox>();
+        private HashSet<int> correctIndices = new HashSet<int>();
+        private HashSet<int> selectedIndices = new HashSet<int>();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string imagesFolder = Application.StartupPath + @"\CaptchaImages";
+            if (!Directory.Exists(imagesFolder) || Directory.GetFiles(imagesFolder, "*.jpg").Length < 9)
+            {
+                CreateCaptchaImages();
+            }
+
+            rbText.Checked = true;
+            GenerateCaptcha();
+            this.BackColor = Color.WhiteSmoke;
+            panelCaptchaImages.Left = (this.ClientSize.Width - panelCaptchaImages.Width) / 2;
+            lblInstruction.Left = (this.ClientSize.Width - lblInstruction.Width) / 2;
+            btnSubmit.Left = panelCaptchaImages.Left;
+            btnRefresh.Left = btnSubmit.Right + 10;
+
+
+        }
+
+        private void rbText_CheckedChanged(object sender, EventArgs e)
+        {
+            lblInstruction.Visible = false;
+            GenerateCaptcha();
+            ClearInputs();
+        }
+
+        private void rbImage_CheckedChanged(object sender, EventArgs e)
+        {
+            GenerateCaptcha();
+            ClearInputs();
+        }
+
+        private void rbMath_CheckedChanged(object sender, EventArgs e)
+        {
+            lblInstruction.Visible = false;
+            GenerateCaptcha();
+            ClearInputs();
+        }
+
+        private void rbCheckbox_CheckedChanged(object sender, EventArgs e)
+        {
+            lblInstruction.Visible = false;
+            GenerateCaptcha();
+            ClearInputs();
+        }
+
+        private void btnSubmit_Click(object sender, EventArgs e)
+        {
+            if (rbImage.Checked)
+            {
+                if (selectedIndices.SetEquals(correctIndices) && correctIndices.Count > 0)
+                {
+                    labelResult.Text = "✅ Correct! Good job!";
+                    labelResult.ForeColor = Color.Green;
+                }
+                else
+                {
+                    labelResult.Text = "❌ Incorrect, try again.";
+                    labelResult.ForeColor = Color.Red;
+                }
+            }
+            else if (rbCheckbox.Checked)
+            {
+                if (cbCaptcha.Checked)
+                {
+                    labelResult.Text = "✅ Correct! Checkbox selected.";
+                    labelResult.ForeColor = Color.Green;
+                }
+                else
+                {
+                    labelResult.Text = "❌ Please check the box.";
+                    labelResult.ForeColor = Color.Red;
+                }
+            }
+            else
+            {
+                if (txtAnswer.Text.Trim() == currentCaptcha)
+                {
+                    labelResult.Text = "✅ Correct!";
+                    labelResult.ForeColor = Color.Green;
+                }
+                else
+                {
+                    labelResult.Text = "❌ Incorrect CAPTCHA!";
+                    labelResult.ForeColor = Color.Red;
+                }
+            }
+        }
+
+        private void btnRefresh_Click(object sender, EventArgs e)
+        {
+            GenerateCaptcha();
+            ClearInputs();
+            labelResult.Text = "";
+        }
+
+        private void ClearInputs()
+        {
+            txtAnswer.Text = "";
+            cbCaptcha.Checked = false;
+            selectedIndices.Clear();
+        }
+
+        private void GenerateCaptcha()
+        {
+            // إخفاء كل حاجة مبدئيًا
+            lblCaptcha.Visible = false;
+            pbCaptcha.Visible = false;
+            cbCaptcha.Visible = false;
+            panelCaptchaImages.Visible = false;
+            lblInstruction.Visible = false;
+            txtAnswer.Visible = false;  // 👈 أخفي صندوق الكتابة
+            txtAnswer.Text = "";        // 🧼 امسح اللي فيه
+
+            if (rbText.Checked)
+            {
+                txtAnswer.Visible = true;
+                GenerateTextCaptcha();
+            }
+            else if (rbImage.Checked)
+            {
+                GenerateImageCaptcha();
+            }
+            else if (rbMath.Checked)
+            {
+                txtAnswer.Visible = true;
+                GenerateMathCaptcha();
+            }
+            else if (rbCheckbox.Checked)
+            {
+                GenerateCheckboxCaptcha();
+            }
+        }
+
+
+        private void GenerateTextCaptcha()
+        {
+            ClearOldCaptcha();
+
+            currentCaptcha = "";
+            string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+            for (int i = 0; i < 6; i++)
+                currentCaptcha += chars[random.Next(chars.Length)];
+
+            lblCaptcha.Text = currentCaptcha;
+            lblCaptcha.Font = new Font("Arial", 24, FontStyle.Bold);
+            lblCaptcha.Visible = true;
+            pbCaptcha.Visible = true;
+        }
+
+        private void GenerateImageCaptcha()
+        {
+            ClearOldCaptcha();
+
+            string imagesFolder = Application.StartupPath + @"\CaptchaImages";
+            string[] allImages = Directory.GetFiles(imagesFolder, "*.jpg");
+
+            if (allImages.Length < 9)
+            {
+                MessageBox.Show("Please add at least 9 images inside CaptchaImages folder.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            panelCaptchaImages.Visible = true;
+            panelCaptchaImages.Enabled = true;
+            panelCaptchaImages.BringToFront();
+
+            correctIndices.Clear();
+            selectedIndices.Clear();
+
+            List<string> selectedImages = new List<string>();
+            while (selectedImages.Count < 9)
+            {
+                string img = allImages[random.Next(allImages.Length)];
+                if (!selectedImages.Contains(img))
+                    selectedImages.Add(img);
+            }
+
+            for (int i = 0; i < 9; i++)
+            {
+                PictureBox pb = new PictureBox();
+                pb.Width = 100;
+                pb.Height = 100;
+                pb.Left = (i % 3) * 110;
+                pb.Top = (i / 3) * 110;
+                pb.SizeMode = PictureBoxSizeMode.StretchImage;
+                pb.Image = Image.FromFile(selectedImages[i]);
+                pb.BorderStyle = BorderStyle.FixedSingle;
+                pb.Tag = i;
+
+                // ✅ مهم جدًا
+                pb.Cursor = Cursors.Hand;
+                pb.Enabled = true;
+                pb.BringToFront();
+                pb.Click += new EventHandler(Pb_Click);
+
+                panelCaptchaImages.Controls.Add(pb);
+                captchaPictures.Add(pb);
+
+                if (Path.GetFileName(selectedImages[i]).ToLower().Contains("apple"))
+                    correctIndices.Add(i);
+            }
+
+            lblInstruction.Text = "👉 Please select all images containing APPLE 🍎";
+            lblInstruction.Font = new Font("Arial", 14, FontStyle.Bold);
+            lblInstruction.Visible = true;
+        }
+
+
+        private void Pb_Click(object sender, EventArgs e)
+        {
+            PictureBox pb = sender as PictureBox;
+            int index = (int)pb.Tag;
+
+            if (selectedIndices.Contains(index))
+            {
+                selectedIndices.Remove(index);
+                pb.BackColor = Color.Transparent;
+                pb.Padding = new Padding(0); // شيل أي تأثير
+            }
+            else
+            {
+                selectedIndices.Add(index);
+                pb.BackColor = Color.LimeGreen;
+                pb.Padding = new Padding(3); // يخلي اللون باين
+            }
+
+            // ✅ للتأكيد خلي الصورة تتحدث
+            pb.Invalidate();
+        }
+
+
+
+
+        private void ClearOldCaptcha()
+        {
+            foreach (var pb in captchaPictures)
+            {
+                panelCaptchaImages.Controls.Remove(pb);
+                pb.Dispose();
+            }
+            captchaPictures.Clear();
+        }
+
+        private void GenerateMathCaptcha()
+        {
+            ClearOldCaptcha();
+
+            int num1 = random.Next(1, 10);
+            int num2 = random.Next(1, 10);
+            currentCaptcha = (num1 + num2).ToString();
+            lblCaptcha.Text = $"{num1} + {num2} = ?";
+            lblCaptcha.Font = new Font("Arial", 24, FontStyle.Bold);
+            lblCaptcha.Visible = true;
+            pbCaptcha.Visible = true;
+        }
+
+        private void GenerateCheckboxCaptcha()
+        {
+            ClearOldCaptcha();
+            cbCaptcha.Checked = false;
+            cbCaptcha.Visible = true;
+        }
+
+        private void CreateCaptchaImages()
+        {
+            string outputFolder = Application.StartupPath + @"\CaptchaImages";
+            Directory.CreateDirectory(outputFolder);
+
+            string[] labels = {
+                "Apple", "Apple", "Apple",
+                "Banana", "Grapes", "Lemon",
+                "Orange", "Strawberry", "Watermelon"
+            };
+
+            for (int i = 0; i < labels.Length; i++)
+            {
+                using (Bitmap bmp = new Bitmap(100, 100))
+                {
+                    using (Graphics g = Graphics.FromImage(bmp))
+                    {
+                        g.Clear(Color.White);
+
+                        using (Font font = new Font("Arial", 12, FontStyle.Bold))
+                        using (Brush brush = new SolidBrush(Color.Black))
+                        {
+                            StringFormat sf = new StringFormat()
+                            {
+                                Alignment = StringAlignment.Center,
+                                LineAlignment = StringAlignment.Center
+                            };
+                            g.DrawString(labels[i], font, brush, new RectangleF(0, 0, bmp.Width, bmp.Height), sf);
+                        }
+                    }
+
+                    string fileName = $"{labels[i].ToLower()}{i + 1}.jpg";
+                    bmp.Save(Path.Combine(outputFolder, fileName), ImageFormat.Jpeg);
+                }
+            }
+
+            Thread.Sleep(500);
+        }
+
+        private void panelCaptchaImages_Paint(object sender, PaintEventArgs e) { }
+    }
+}
